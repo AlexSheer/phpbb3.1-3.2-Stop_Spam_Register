69aef2edd23433c5c2b57fc0134b643f9a466aba
AlexSheer <alex_sheer@inbox.lv>
2017-04-15 20:14:41 +0300

    Create stopregister.php

---
+++ language/en/stopregister.php
@@ -0,0 +1,35 @@
+<?php
+/**
+*
+* @package phpBB Extension - Stop spamer register
+* @copyright (c) 2017 Sheer
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_STOPFORUMSPAM'			=> 'Checking from database www.stopforumspam.com',
+	'IP_STOP'					=> 'Your IP address is blacklisted in <a href="http://www.stopforumspam.com" target = "_blank">www.stopforumspam.com</a>',
+	'NICK_STOP'					=> 'Username is blacklisted ih <a href="http://www.stopforumspam.com" target = "_blank">www.stopforumspam.com</a><br />Попробуйте подобрать другое',
+	'EMAIL_STOP'				=> 'E-mail v <a href="http://www.stopforumspam.com" target = "_blank">www.stopforumspam.com</a><br />Попробуйте использовать другой',
+	'REPORT_STOP'				=> 'If you believe that this was a misunderstanding, please report this %sадминистратору%s',
+	'ALLOW_STOPFORUMSPAM'		=> 'Enable',
+	'ALLOW_STOPFORUMSPAM_EXPLAIN'=> 'If enabled, the IP address, username and e-mail address will be checked from database www.stopforumspam.com<br />Disabling <b>all three</b> of the following options is equivalent to disabling this setting.',
+	'ALLOW_REG_LOG'				=> 'Enable logs',
+	'CHECK_SPAM_IP'				=> 'Check by IP address',
+	'CHECK_USERNAME'			=> 'Check by username',
+	'CHECK_SPAM_EMAIL'			=> 'Check by e-mail',
+));
