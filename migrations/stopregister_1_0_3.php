7b6d51df31d02e5ffd76aefc19cc22a1578dc974
AlexSheer <alex_sheer@inbox.lv>
2019-02-03 20:19:29 +0200

    Add files via upload

---
+++ migrations/stopregister_1_0_3.php
@@ -0,0 +1,43 @@
+<?php
+/**
+*
+* @package phpBB Extension - Stop spamer register
+* @copyright (c) 2018 Sheer
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+namespace sheer\stopregister\migrations;
+
+class stopregister_1_0_3 extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return isset($this->config['stopregister_version']) && version_compare($this->config['stopregister_version'], '1.0.3', '>=');
+	}
+
+	static public function depends_on()
+	{
+		return array('\sheer\stopregister\migrations\stopregister_1_0_2');
+	}
+
+	public function update_schema()
+	{
+		return array(
+		);
+	}
+	public function revert_schema()
+	{
+		return array(
+		);
+	}
+
+	public function update_data()
+	{
+		return array(
+			// Current version
+			array('config.update', array('stopregister_version', '1.0.3')),
+			array('config.add', array('stopreglog_last_run', '0')),
+			array('config.add', array('sfsl_expire_days', '7')),
+		);
+	}
+}
