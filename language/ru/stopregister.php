dd44b62e42110a1c3183dd9d4089572306217690
AlexSheer <alex_sheer@inbox.lv>
2019-09-05 12:33:20 +0300

    Update stopregister.php

--- language/ru/stopregister.php
+++ language/ru/stopregister.php
@@ -22,10 +22,10 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'ACP_STOPFORUMSPAM'			=> 'Проверка по БД www.stopforumspam.com',
-	'IP_STOP'					=> 'Ваш IP-адрес внесен в черный список сервиса <a href="http://www.stopforumspam.com" target = "_blank">www.stopforumspam.com</a>',
+	'IP_STOP'					=> 'Ваш IP-адрес внесен в черный список сервиса <a href="https://www.stopforumspam.com" target = "_blank">www.stopforumspam.com</a>',
 	'NICK_STOP'					=> 'Выбранное вами имя внесено в черный список сервиса <a href="http://www.stopforumspam.com" target = "_blank">www.stopforumspam.com</a><br />Попробуйте подобрать другое',
-	'EMAIL_STOP'				=> 'Указанный адрес e-mail внесен в черный список сервиса <a href="http://www.stopforumspam.com" target = "_blank">www.stopforumspam.com</a><br />Попробуйте использовать другой',
-	'REPORT_STOP'				=> 'Если вы считаете, что это произошло по недоразумению, сообщите об этом %sадминистратору%s',
+	'EMAIL_STOP'				=> 'Указанный адрес e-mail внесен в черный список сервиса <a href="https://www.stopforumspam.com" target = "_blank">www.stopforumspam.com</a><br />Попробуйте использовать другой',
+	'REPORT_STOP'				=> 'Если вы считаете, что это произошло по недоразумению,  отправьте <a href="https://www.stopforumspam.com/removal" target = "_blank">запрос на удаление данных из базы данных stopforumspam.com</a>',
 	'ALLOW_STOPFORUMSPAM'		=> 'Включить',
 	'ALLOW_STOPFORUMSPAM_EXPLAIN'=> 'Если включено, то IP-адрес, имя пользователя и адрес e-mail будут проверены по базе данных www.stopforumspam.com<br />Отключение <b>всех трех</b> нижеследующих параметров равносильно отключению этого параметра.',
 	'ALLOW_REG_LOG'				=> 'Вести лог регистраций',
